4e943cb5f37f22de3ed0a3b3dfe99b16eef9771f
bleck-ps <120287087+bleck-ps@users.noreply.github.com>
2023-01-13 16:53:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var lainfo = document.getElementById('lainfo');
+var latarjeta = document.getElementById('latarjeta');
+var front = document.getElementById('front');
+
+var tamano = front.offsetHeight;
+lainfo.style.marginTop = tamano + 'px';
